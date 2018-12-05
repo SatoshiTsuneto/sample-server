80724a150b9a7b13c621af258f8ac2278b5b57d1
satotune <satotune2718@gmail.com>
2018-12-05 15:57:28 +0900

    ソースのリファクタリング

--- main.go
+++ main.go
@@ -10,25 +10,18 @@ import (
 
 // POSTリクエストに対する処理
 func postJsonHandler(rw http.ResponseWriter, req *http.Request) {
-	var response string
-	defer func() {
-		_, err := json.Marshal(response)
-		if err != nil {
-			fmt.Println(err)
-		}
-		rw.Header().Set("Content-Type", "application/json")
-	}()
+	// リクエストの設定
+	rw.Header().Set("Content-Type", "application/json")
 
 	// メソッドがPOST確認
 	if req.Method != "POST" {
-		response = "Method Not POST."
+		fmt.Fprint(rw, "Method Not POST.")
 		return
 	}
 
 	body, err := ioutil.ReadAll(req.Body)
 	if err != nil {
-		response = err.Error()
-		fmt.Println(err.Error())
+		fmt.Fprint(rw, err.Error())
 		return
 	}
 
@@ -36,8 +29,7 @@ func postJsonHandler(rw http.ResponseWriter, req *http.Request) {
 	var input goMySql.User
 	err = json.Unmarshal(body, &input)
 	if err != nil {
-		response = err.Error()
-		fmt.Println(err.Error())
+		fmt.Fprint(rw, err.Error())
 		return
 	}
 
@@ -53,17 +45,21 @@ func postJsonHandler(rw http.ResponseWriter, req *http.Request) {
 
 // GETリクエストに対する処理
 func getJsonHandler(rw http.ResponseWriter, req *http.Request) {
+	// リクエストの設定
+	rw.Header().Set("Content-Type", "application/json")
+
 	// DBからデータを取得
 	users := goMySql.SqlRead()
 
 	// 構造体をJSONに変換
-	response, _ := json.Marshal(users)
+	response, err := json.Marshal(users)
+	if err != nil {
+		fmt.Fprint(rw, err.Error())
+		return
+	}
 
 	// クライアントへ取得したデータを投げる
-	defer func() {
-		rw.Header().Set("Content-Type", "application/json")
-		fmt.Fprint(rw, string(response))
-	}()
+	fmt.Fprint(rw, string(response))
 }
 
 func main() {
@@ -71,5 +67,5 @@ func main() {
 	http.HandleFunc("/post", postJsonHandler)
 	http.HandleFunc("/get", getJsonHandler)
 	// port8080で受け付ける
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":9999", nil)
 }
